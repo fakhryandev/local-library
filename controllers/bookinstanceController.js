baaccbd6b500eb5b0e7b357b56a0b6f677784c1f
Muhammad Fakhryan <fakhryandev@gmail.com>
2023-03-06 22:13:24 +0700

    add new book instance

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,4 +1,6 @@
 const BookInstance = require("../models/bookinstance");
+const Book = require("../models/book");
+const { body, validationResult } = require("express-validator");
 
 exports.bookinstance_list = (req, res, next) => {
   BookInstance.find()
@@ -44,20 +46,67 @@ exports.bookinstance_delete_post = (req, res) => {
 };
 
 exports.bookinstance_create_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: BookInstance create GET");
+  Book.find({}, "title").exec((err, books) => {
+    if (err) {
+      return next(err);
+    }
+
+    res.render("bookinstance_form", {
+      title: "Create BookInstance",
+      book_list: books,
+    });
+  });
 };
 
-exports.bookinstance_create_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: BookInstance create POST");
-};
+exports.bookinstance_create_post = [
+  body("book", "Book must be specified").trim().isLength({ min: 1 }).escape(),
+  body("imprint", "Imprint must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("status").escape(),
+  body("due_back", "Invalid date")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
 
-exports.bookinstance_create_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: BookInstance create POST");
-};
+  (req, res, next) => {
+    const errors = validationResult(req);
 
-exports.bookinstance_create_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: BookInstance create POST");
-};
+    const bookinstance = new BookInstance({
+      book: req.body.book,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back,
+    });
+
+    if (!errors.isEmpty()) {
+      Book.find({}, "title").exec(function (err, books) {
+        if (err) {
+          return next(err);
+        }
+
+        res.render("bookinstance_form", {
+          title: "Create BookInstance",
+          book_list: books,
+          selected_book: bookinstance.book._id,
+          errors: errors.array(),
+          bookinstance,
+        });
+      });
+
+      return;
+    }
+
+    bookinstance.save((err) => {
+      if (err) {
+        return next(err);
+      }
+
+      res.redirect(bookinstance.url);
+    });
+  },
+];
 
 exports.bookinstance_update_get = (req, res) => {
   res.send("NOT IMPLEMENTED: BookInstance update GET");
